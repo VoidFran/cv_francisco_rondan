801763d72af00c0552c9f6630391bb05e3f492b5
VoidFran <franciscof2menosf1@gmail.com>
2023-06-27 15:52:35 -0300

    watefac
    Signed-off-by: VoidFran <franciscof2menosf1@gmail.com>

---
+++ javascript/programacion.js
@@ -0,0 +1,14 @@
+window.onload = function cargar_pagina() {
+    // Al cargar la página se cargan los datos del Json en el Local Storage
+    guardar_local_storage()
+}
+
+function guardar_local_storage() {
+    // Al cargar la página se guardan los datos del Json que esta en el Local Storage en la Tabla
+    leer_local_storge()
+}
+
+function leer_local_storge() {
+    // Recorre la Lista donde estan almacenados los Jugadores
+    
+}
